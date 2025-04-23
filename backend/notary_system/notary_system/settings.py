a3a787de208a4fabb0fb0d5b395245a847329ccd
Shokhrukh Davlatmamadov <shokhrukh.davlatmamadov_2025@ucentralasia.org>
2025-04-24 00:55:17 +0600

    some changes

--- backend/notary_system/notary_system/settings.py
+++ backend/notary_system/notary_system/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'notary_db'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
