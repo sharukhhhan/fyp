4b0db4acb77885c674f1cd5b0128cb4309f536b8
Shokhrukh <55704712+sharukhhhan@users.noreply.github.com>
2025-05-08 23:09:22 +0600

    Update settings.py: try to fix

--- backend/notary_system/notary_system/settings.py
+++ backend/notary_system/notary_system/settings.py
@@ -19,7 +19,7 @@ SERVER_PORT = os.environ.get('SERVER_PORT', '8081')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 CSRF_TRUSTED_ORIGINS = ['https://3.92.108.217']
-FORCE_SCRIPT_NAME = '/notary'
+# FORCE_SCRIPT_NAME = '/notary'
 STATIC_URL = '/static/' 
 STATIC_ROOT = '/app/staticfiles/'
 # Application definition
