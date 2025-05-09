e3ca993d75c0efada1a37723de536fd16751a52b
Shokhrukh <55704712+sharukhhhan@users.noreply.github.com>
2025-05-09 17:09:48 +0600

    Update settings.py: allowed https

--- backend/notary_system/notary_system/settings.py
+++ backend/notary_system/notary_system/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-change-me
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SERVER_PORT = os.environ.get('SERVER_PORT', '8081')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
